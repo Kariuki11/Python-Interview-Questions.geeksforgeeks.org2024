4a0d0eb6ba23331aa4235302a706d95e912a21cc
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:04:23 +0300

    Python Questions

---
+++ Q1.py
@@ -0,0 +1,10 @@
+1. What is Python? List some popular applications of Python in the world of technology.
+
+# Python is a widely-used general-purpose, high-level programming language. It was created by Guido van Rossum in 1991 and further developed by the Python Software Foundation. It was designed with an emphasis on code readability, and its syntax allows programmers to express their concepts in fewer lines of code.
+# It is used for:
+
+            # System Scripting
+            # Web Development
+            # Game Development
+            # Software Development
+            # Complex Mathematics
