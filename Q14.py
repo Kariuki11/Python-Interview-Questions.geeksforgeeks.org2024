38167d30b288df690b88ca931e44af01abe7dee1
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:39:28 +0300

    changes

---
+++ Q14.py
@@ -0,0 +1,4 @@
+14. Difference between for loop and while loop in Python
+
+            #The “for” Loop is generally used to iterate through the elements of various collection types such as List, Tuple, Set, and Dictionary. Developers use a “for” loop where they have both the conditions start and the end. Whereas, the “while” loop is the actual looping feature that is used in any other programming language. Programmers use a Python while loop where they just have the end conditions.
+
---
+++ Q16.py
@@ -0,0 +1,4 @@
+16. What are *args and **kwargs?
+
+       # To pass a variable number of arguments to a function in Python, use the special syntax *args and **kwargs in the function specification. Both are to send a variable-length argument list. The syntax *args is used to pass a non-keyworded, variable-length argument list.
+
