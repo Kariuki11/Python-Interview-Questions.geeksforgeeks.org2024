27a3163bb34ae719b645d6279d78105b82584db2
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:21:20 +0300

    CHANGES

---
+++ Q7.py
@@ -0,0 +1,5 @@
+What is the difference between a Set and Dictionary?
+
+        # The set is an unordered collection of data types that is iterable, mutable and has no duplicate elements.
+        # A dictionary in Python is an ordered collection of data values, used to store data values like a map.
+
---
+++ Q8.py
@@ -0,0 +1,7 @@
+ What is List Comprehension? Give an Example.
+ 
+        #List comprehension is a syntax construction to ease the creation of a list based on existing iterable.
+        
+                    #For Example:
+
+                            #my_list = [i for i in range(1, 10)]
