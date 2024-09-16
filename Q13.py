a6ba492848ac652e77011011c646036c044c5641
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:34:18 +0300

    changes

---
+++ Q13.py
@@ -0,0 +1,13 @@
+13. What is swapcase function in Python?
+
+        #It is a string’s function that converts all uppercase characters into lowercase and vice versa. It is used to alter the existing case of the string. This method creates a copy of the string which contains all the characters in the swap case.
+        
+#For Example:
+string = "GeeksforGeeks"
+string.swapcase() ---> "gEEKSFORgEEKS"
+
+string = "KariukiKenn"
+string.swapcase() ---> "kARIUKIkENN"
+
+
+
