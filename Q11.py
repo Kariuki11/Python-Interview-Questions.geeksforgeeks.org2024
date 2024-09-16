97fbe2941454cd478d7fa59e1a7fa9d6a783c14e
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:30:47 +0300

    changes

---
+++ Q11.py
@@ -0,0 +1,7 @@
+What is the difference between / and // in Python?
+
+    #/ represents precise division (result is a floating point number) whereas // represents floor division (result is an integer).
+        
+# For Example:
+5//2 = 2
+5/2 = 2.5
---
+++ Q12.py
@@ -0,0 +1,7 @@
+ How is Exceptional handling done in Python?
+ 
+        # There are 3 main keywords i.e. try, except, and finally which are used to catch exceptions and handle the recovering mechanism accordingly. 
+                # Try is the block of a code that is monitored for errors. 
+                # Except block gets executed when an error occurs.
+
+                # The beauty of the final block is to execute the code after trying for an error. This block gets executed irrespective of whether an error occurred or not. Finally, block is used to do the required cleanup activities of objects/variables.
