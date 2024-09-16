881ec3f06ff84f69b7960ff12bb62fa6a6f8895a
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:13:58 +0300

    changes

---
+++ Q4.py
@@ -0,0 +1,4 @@
+. What does the ‘#’ symbol do in Python?
+
+
+‘#’ is used to comment on everything that comes after on the line.
---
+++ Q6.py
@@ -0,0 +1,4 @@
+6. How are arguments passed by value or by reference in Python?
+
+            # Everything in Python is an object and all variables hold references to the objects. The reference values are according to the functions; as a result, you cannot change the value of the references. However, you can change the objects if it is mutable.
+
