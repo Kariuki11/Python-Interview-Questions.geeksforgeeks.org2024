3d6d43bfc46a0d584baabb3bc382708019becf2e
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:40:30 +0300

    changes

---
+++ Q17.py
@@ -0,0 +1,4 @@
+17. Is Indentation Required in Python?
+
+            #Yes, indentation is required in Python. A Python interpreter can be informed that a group of statements belongs to a specific block of code by using Python indentation. Indentations make the code easy to read for developers in all programming languages but in Python, it is very important to indent the code in a specific order.
+
