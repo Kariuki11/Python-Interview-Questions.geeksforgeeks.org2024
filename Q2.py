8530514af8152841ff777b73bc52b4d5b2101d21
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:07:24 +0300

    changes

---
+++ Q2.py
@@ -0,0 +1,11 @@
+What are the benefits of using Python language as a tool in the present scenario?
+
+#The following are the benefits of using Python language:
+                # Object-Oriented Language
+                # High-Level Language
+                # Dynamically Typed language
+                # Extensive support Libraries
+                # Presence of third-party modules
+                # Open source and community development
+                # Portable and Interactive
+                # Portable across Operating systems
