2053019f3f6239fac7ac79b341d9ed6be9066300
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:42:33 +0300

    changes

---
+++ Q19.py
@@ -0,0 +1,5 @@
+19. What is docstring in Python?
+Python documentation strings (or docstrings) provide a convenient way of associating documentation with Python modules, functions, classes, and methods.
+
+Declaring Docstrings: The docstrings are declared using ”’triple single quotes”’ or “””triple double quotes””” just below the class, method, or function declaration. All functions should have a docstring.
+Accessing Docstrings: The docstrings can be accessed using the __doc__ method of the object or using the help function.
