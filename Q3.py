0355661ebe86d1e5756ddf1fb6fd647b90d404dd
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:10:36 +0300

    changes

---
+++ Q3.py
@@ -0,0 +1,3 @@
+ Is Python a compiled language or an interpreted language?
+ 
+        #Actually, Python is a partially compiled language and partially interpreted language. The compilation part is done first when we execute our code and this will generate byte code internally this byte code gets converted by the Python virtual machine(p.v.m) according to the underlying platform(machine+operating system).
