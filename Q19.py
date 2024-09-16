950476a78488679be66e790170304a97a83cb076
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-16 15:44:31 +0300

    changes

--- Q19.py
+++ Q19.py
@@ -1,5 +1,8 @@
 19. What is docstring in Python?
-Python documentation strings (or docstrings) provide a convenient way of associating documentation with Python modules, functions, classes, and methods.
+            #Python documentation strings (or docstrings) provide a convenient way of associating documentation with Python modules, functions, classes, and methods.
 
-Declaring Docstrings: The docstrings are declared using ”’triple single quotes”’ or “””triple double quotes””” just below the class, method, or function declaration. All functions should have a docstring.
-Accessing Docstrings: The docstrings can be accessed using the __doc__ method of the object or using the help function.
+            #Declaring Docstrings: The docstrings are declared using ”’triple single quotes”’ or “””triple double quotes””” just below the class, method, or function declaration. All functions should have a docstring.
+            #Accessing Docstrings: The docstrings can be accessed using the __doc__ method of the object or using the help function.
+            
+            
+            #The docstrings are not assigned to any variable and therefore, at times, it is not necessary to use the print statement to display them.
---
+++ Q20.py
@@ -0,0 +1,5 @@
+20. What is a dynamically typed language?
+#Typed languages are the languages in which we define the type of data type and it will be known by the machine at the compile-time or at runtime. Typed languages can be classified into two categories:
+
+        #Statically typed languages: In this type of language, the data type of a variable is known at the compile time which means the programmer has to specify the data type of a variable at the time of its declaration. 
+        #Dynamically typed languages: These are the languages that do not require any pre-defined data type for any variable as it is interpreted at runtime by the machine itself. In these languages, interpreters assign the data type to a variable at runtime depending on its value.
