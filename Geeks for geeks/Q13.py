14f3187b2bec54c9053f8eed645a134aa891b4d5
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-17 22:55:01 +0300

    changes

--- Geeks for geeks/Q13.py
+++ Geeks for geeks/Q13.py
@@ -1,13 +1,11 @@
 13. What is swapcase function in Python?
 
-        #It is a string’s function that converts all uppercase characters into lowercase and vice versa. It is used to alter the existing case of the string. This method creates a copy of the string which contains all the characters in the swap case.
+        #It is a string’s function that converts all uppercase characters into lowercase and vice versa. It is used to alter the existing case of the string.
+                # This method creates a copy of the string which contains all the characters in the swap case.
         
 #For Example:
 string = "GeeksforGeeks"
 string.swapcase() ---> "gEEKSFORgEEKS"
 
 string = "KariukiKenn"
-string.swapcase() ---> "kARIUKIkENN"
-
-
-
+string.swapcase() ---> "kARIUKIkENN".
--- Geeks for geeks/Q15.py
+++ Geeks for geeks/Q15.py
@@ -1,3 +1,4 @@
 15. Can we Pass a function as an argument in Python?
 
-               # Yes, Several arguments can be passed to a function, including objects, variables (of the same or distinct data types), and functions. Functions can be passed as parameters to other functions because they are objects. Higher-order functions are functions that can take other functions as arguments.
+               # Yes, Several arguments can be passed to a function, including objects, variables (of the same or distinct data types), and functions.
+               # Functions can be passed as parameters to other functions because they are objects. Higher-order functions are functions that can take other functions as arguments.
